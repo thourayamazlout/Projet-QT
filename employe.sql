a16f1877e35cab2d5515580949fc7fcd9808dd97
ahmedouertani <49523156+ahmedouertani@users.noreply.github.com>
2020-04-27 13:47:21 +0100

    Add files via upload

---
+++ employe.sql
@@ -0,0 +1,27 @@
+--------------------------------------------------------
+--  Fichier créé - lundi-avril-27-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table EMPLOYE
+--------------------------------------------------------
+
+  CREATE TABLE "AHMED"."EMPLOYE" 
+   (	"ID" VARCHAR2(20 BYTE), 
+	"NOM" VARCHAR2(20 BYTE), 
+	"PRENOM" VARCHAR2(20 BYTE), 
+	"FONCTION" VARCHAR2(20 BYTE), 
+	"SALAIRE" NUMBER
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+REM INSERTING into AHMED.EMPLOYE
+SET DEFINE OFF;
+Insert into AHMED.EMPLOYE (ID,NOM,PRENOM,FONCTION,SALAIRE) values ('aaaaa','aqefzca','aqefaq','zfaca','51555');
+Insert into AHMED.EMPLOYE (ID,NOM,PRENOM,FONCTION,SALAIRE) values ('afdea','Seif','ben salah','info','5254');
+Insert into AHMED.EMPLOYE (ID,NOM,PRENOM,FONCTION,SALAIRE) values ('45245','aymen','dcaz','afq','8728');
+Insert into AHMED.EMPLOYE (ID,NOM,PRENOM,FONCTION,SALAIRE) values ('azerty','qfcseq','sdrbvgesr','esbsr','834');
+Insert into AHMED.EMPLOYE (ID,NOM,PRENOM,FONCTION,SALAIRE) values ('azdaz','vvvvv','azfc','zeqvzeqg','8287');
+Insert into AHMED.EMPLOYE (ID,NOM,PRENOM,FONCTION,SALAIRE) values ('azedaz','azfq','aefv','eqvfgza','454');
+Insert into AHMED.EMPLOYE (ID,NOM,PRENOM,FONCTION,SALAIRE) values ('azdaz','vvvvv','azfc','zeqvzeqg','8287');
---
+++ veterinaire.sql
@@ -0,0 +1,36 @@
+--------------------------------------------------------
+--  Fichier créé - lundi-avril-27-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table VETERINAIRE
+--------------------------------------------------------
+
+  CREATE TABLE "AHMED"."VETERINAIRE" 
+   (	"ID" VARCHAR2(20 BYTE), 
+	"NOM" VARCHAR2(20 BYTE), 
+	"PRENOM" VARCHAR2(20 BYTE), 
+	"SPECIALITE" VARCHAR2(20 BYTE), 
+	"DIPLOME" DATE, 
+	"EXP" NUMBER
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+REM INSERTING into AHMED.VETERINAIRE
+SET DEFINE OFF;
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('azdazd','afaef','aefae','aefcae',to_date('03/01/00','DD/MM/RR'),'0');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('ihi','qdvseqg','qegvqAE','QEDGVQAE',to_date('03/01/00','DD/MM/RR'),'0');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('AAAA','Adem','Gaied','Info',to_date('01/01/05','DD/MM/RR'),'2');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('qqqq','Adem','Gaied','aqdz',to_date('03/01/00','DD/MM/RR'),'0');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('qsfaq','azf','azf','azf',to_date('04/01/00','DD/MM/RR'),'8');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('adz','adddd','azdaz','dazf',to_date('01/01/97','DD/MM/RR'),'55');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('vwdqs','vwsdv','dwsvs','qdwvs',to_date('03/01/00','DD/MM/RR'),'44');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('azzert','azdca','aqvfaz','eqgvzqe',to_date('03/01/00','DD/MM/RR'),'0');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('azzert','azdca','aqvfaz','eqgvzqe',to_date('03/01/00','DD/MM/RR'),'0');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('azzert','azdca','aqvfaz','eqgvzqe',to_date('03/01/00','DD/MM/RR'),'0');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('azzert','azdca','aqvfaz','eqgvzqe',to_date('03/01/00','DD/MM/RR'),'0');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('azdaz','azfcaz','azcfaz','azfcaq',to_date('03/01/00','DD/MM/RR'),'552');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('azcfa','azert','daz','acfz',to_date('03/01/00','DD/MM/RR'),'55');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('sgvesz','fza','eqgv','esgvzse',to_date('04/01/00','DD/MM/RR'),'77');
+Insert into AHMED.VETERINAIRE (ID,NOM,PRENOM,SPECIALITE,DIPLOME,EXP) values ('aza','azeaz','azeaz','dqsd',to_date('03/01/00','DD/MM/RR'),'5');
